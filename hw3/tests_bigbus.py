48c1bc837189d1d584a3cafc86e35c3e811e2e25
Vidal Anguiano Jr <vidal.anguiano@gmail.com>
2019-05-08 15:50:09 -0500

    red

--- hw3/tests_bigbus.py
+++ hw3/tests_bigbus.py
@@ -2,7 +2,7 @@ import datetime as dt
 
 import pytest
 
-from bigbus import UserInt, get_dates_10d_out
+from bigbus import UserInt, Seller, get_dates_10d_out
 
 @pytest.mark.parametrize('command', ['Sell', 'Issue Refund', 'Report', 'Quit'])
 def test_is_valid_command(command):
@@ -16,3 +16,13 @@ def test_date_select_within_10days():
 
     next_day =  (dt.datetime.today() + dt.timedelta(days=1)).strftime('%m-%d-%Y')
     assert next_day in next_10_days
+
+
+def test_get_ride_and_rider_info():
+    seller = Seller()
+    seller.collect_ticket_sale_details()
+
+    assert seller.ride_date
+    assert seller.bus_route
+    assert seller.ticket_quant
+    assert seller.rider_name
