f9c1c5b5e9740387535d0f1915e882f514aef07d
Vidal Anguiano Jr <vidal.anguiano@gmail.com>
2019-05-08 20:34:06 -0500

    read

--- hw3/tests_bigbus.py
+++ hw3/tests_bigbus.py
@@ -1,8 +1,11 @@
 import datetime as dt
 
 import pytest
+import psycopg2
+import sqlalchemy
 
-from bigbus import UserInt, Seller, get_dates_10d_out
+from bigbus import UserInt, Seller, DBSess, get_dates_10d_out
+from bigbus_orm import Tickets
 
 EXAMPLE_SALES = [{'ride_date': '07-01-2019',
                   'bus_route': 'red',
@@ -12,6 +15,8 @@ EXAMPLE_SALES = [{'ride_date': '07-01-2019',
                    'bus_route': 'blue',
                    'ticket_quant': 4,
                    'rider_name': 'test_case'}]
+
+dbsess = DBSess().getInstance()
 
 @pytest.mark.parametrize('command', ['Sell', 'Issue Refund', 'Report', 'Quit'])
 def test_is_valid_command(command):
@@ -46,3 +51,21 @@ def test_create_tickets_from_sale_details():
 
     assert len(seller.tickets) == details['ticket_quant']
     assert seller.tickets[0]['rider_name'] == details['rider_name']
+
+
+def test_ticket_sale_processes_successfully():
+    seller = Seller()
+    details = EXAMPLE_SALES[1]
+    seller.collect_ticket_sale_details(test=True, details=details)
+    seller.prepare_tickets()
+    seller.process_sale(dbsess)
+
+    results = dbsess.query(Ticket)\
+                    .filter(Tickets.rider_name == 'test_case')\
+                    .filter(Tickets.ride_date == '07-02-2019')\
+                    .filter(Tickets.bus_route == 'blue')\
+                    .all()
+
+    print(results)
+
+    assert len(results) == 4
